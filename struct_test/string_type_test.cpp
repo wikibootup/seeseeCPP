e8b49401a8495b8f4c20b19c51ac3b5d94524c2b
wikibootup <wikibootup@gmail.com>
2014-08-28 00:10:57 +0900

    added convinient string append for c++

---
+++ struct_test/string_type_test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string des = "apple";
+	string src1 = " is ";
+	string src2 = "delicious";
+	a += b += c;
+
+	cout << des << endl;
+	return 0;
+}
